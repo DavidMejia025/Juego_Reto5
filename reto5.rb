4a4536ff3f028a4c9f465602027cd2a229d0a5be
OEM Configuration (temporary user) <davidndy@gmail.com>
2018-01-06 11:48:26 -0500

    Reto 5 Skelleton done, plus the basic functions to work. Gathering information from a text file done asking for answers done comparing answers done selecting random questions done

--- reto5.rb
+++ reto5.rb
@@ -1,15 +1,65 @@
 # Juego Reto 5.
 # En este juego se le permitira a un jugador exponerse al reto de contestar ciertas preguntas
 
-
-def read_question(index)
-
+require "pry"
+def read_data()
+	text = File.readlines("data.txt")
+	options = []
+	text.length.times do |i|
+		options << i
+	end
+	game = find_question(text, options)
 end
 
-def read_answer(index)
-	
+def find_question(text, array)
+	ind = array.sample
+	if text[ind].include? "#"
+		question = text[ind]
+		answer = text[ind+1]
+		
+	else
+		question = text[ind-1]
+		answer = text[ind]
+	end
+	[question, answer]
 end
 
-def is_value?
-	
+def is_value?(real_answer, player_answer)
+	p real_answer
+	p player_answer
+	real_answer == player_answer ? true : false
 end
+
+def pre_game()
+	puts "Bienvenido al juego Reto 5"
+	puts "Estas preparado para jugar?"
+	puts "..........................."
+	puts "..........................."
+	puts "Quieres empezar ahora Y/N: "
+	start = gets.chomp
+end
+
+def round(question)
+	puts "* Ahora viene tu pregunta: "
+	puts "Cual es #{question}."
+	puts "Ahora es tu turno:"
+end
+#---------------- Game initialization --------------------
+def game()
+	start = pre_game()
+	if  start == "y" 
+		finish = false
+		game = read_data()
+		while finish == false
+			round(game[0])
+			answer = gets.chomp
+			is_value?(game[1], answer)? finish=true : finish=false
+			p finish
+		end
+	else
+		game()
+	end
+end
+game()
+read_data(1)
+p is_value?(1,2)
