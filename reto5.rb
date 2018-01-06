a012f77c8f630b1a7b01aafe11b948e6b9f58eee
OEM Configuration (temporary user) <davidndy@gmail.com>
2018-01-05 21:30:17 -0500

    Reto 5 game create the ruby file and define the functions

---
+++ reto5.rb
@@ -0,0 +1,15 @@
+# Juego Reto 5.
+# En este juego se le permitira a un jugador exponerse al reto de contestar ciertas preguntas
+
+
+def read_question(index)
+
+end
+
+def read_answer(index)
+	
+end
+
+def is_value?
+	
+end
